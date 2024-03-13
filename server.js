f7cd83d524c5e92c3c9da1347afcfb7612f64884
leo77534 <zhaofag@student.unimelb.edu.au>
2024-03-13 14:35:17 +1100

    Feat: Create issue for server

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get("/api/issues", (req, res) => {
   res.json(issue);
 });
 
+app.post("/api/issues", (req, res) => {
+  issue = req.body;
+  console.log("Created Issue:", issue);
+  res.json({ message: "Issue created successfully", issue });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
