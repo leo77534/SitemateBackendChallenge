0062c65e4faade2172d738123f48ab72d99a44fb
leo77534 <zhaofag@student.unimelb.edu.au>
2024-03-13 14:36:49 +1100

    Feat: Update issus for server

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.post("/api/issues", (req, res) => {
   res.json({ message: "Issue created successfully", issue });
 });
 
+app.put("/api/issues", (req, res) => {
+  issue = req.body;
+  console.log("Updated Issue:", issue);
+  res.json({ message: "Issue updated successfully", issue });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
